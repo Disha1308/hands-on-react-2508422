b07ad49ee65c630885c4af3901023ca3cb6304c7
Disha Agrawal <dishaagrawal1358@gmail.com>
2023-11-09 15:31:50 +0000

    different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Welcome from "./Home/welcome";
+
 function App() {
   const name = 'StarGazers'
   return (
@@ -5,6 +7,8 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
+          <Welcome name="Star Disha"/>
+          <Welcome name="Disha" />
           <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
